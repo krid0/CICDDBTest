d7f48cc0ac6fe5f0ffc8dd1218da786ffc7f5d40
krid0 <167877404+krid0@users.noreply.github.com>
2024-09-05 09:43:26 +0800

    Add files via upload

--- dbo/Tables/products.sql
+++ dbo/Tables/products.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[products] (
     [product_name] NVARCHAR (50) NULL,
-    [price]        INT           NULL
+    [price]        INT           NULL, 
+    [CreatedDetails] NCHAR(10) NULL
 );
 
