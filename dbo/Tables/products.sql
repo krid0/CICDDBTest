87e74365bae07ed616e3e25527782dd71db67d86
krid0 <167877404+krid0@users.noreply.github.com>
2024-09-06 11:42:02 +0800

    Add files via upload

--- dbo/Tables/products.sql
+++ dbo/Tables/products.sql
@@ -2,6 +2,7 @@
     [product_name] NVARCHAR (50) NULL,
     [price]        INT           NULL, 
     [CreatedDetails] NCHAR(10) NULL, 
-    [QTY] INT NULL
+    [QTY] INT NULL, 
+    [QTY2] INT NULL
 );
 
